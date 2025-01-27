5907c56d6d60c9c2be11023faa2afac87bf3c491
lufonius <fonjallaz97@gmail.com>
2025-01-27 12:33:15 +0100

    extract default dialog component

---
+++ src/components/dialog.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { Dialog as HeadlessUIDialog, DialogPanel, DialogBackdrop } from '@headlessui/react';
+
+export const Dialog: React.FC<{
+  isOpen: boolean,
+  onClosed: () => void,
+  children: any
+}> = ({ isOpen, onClosed, children }) => {
+
+  return (
+    <HeadlessUIDialog open={isOpen} onClose={() => onClosed()}
+        transition
+        className="relative z-50 transition duration-300 ease-out">
+        <DialogBackdrop className="fixed inset-0 bg-black/30 backdrop-blur-sm backdrop-brightness-50" />
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+            <DialogPanel className="max-w-lg space-y-4 bg-gray-800 p-12 rounded">
+            {children}
+            </DialogPanel>
+        </div>
+    </HeadlessUIDialog>
+  );
+}
+
+export const DialogTitle: React.FC<{children: any}> = ({children}) => {
+    return (<h3 className="font-bold text-2xl">{children}</h3>);
+};
+
+export const DialogActions: React.FC<{children: any}> = ({children}) => {
+    return (<div className="flex gap-4 items-center mt-4">{children}</div>);
+};
--- src/components/download-photos-dialog.tsx
+++ src/components/download-photos-dialog.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import { Dialog, DialogPanel, DialogTitle, Description, DialogBackdrop } from '@headlessui/react';
-import dayjs from 'dayjs';
 import LinkButton from "./link-button";
 import { Cube } from "../model/cube";
+import { Dialog, DialogActions, DialogTitle } from "./dialog";
 
 const DownloadPhotosDialog: React.FC<{
   isOpen: boolean,
@@ -11,24 +10,14 @@ const DownloadPhotosDialog: React.FC<{
 }> = ({ isOpen, cube, onClosed }) => {
 
   return (
-    <Dialog open={isOpen} onClose={() => onClosed()}
-        transition
-        className="relative z-50 transition duration-300 ease-out">
-        <DialogBackdrop className="fixed inset-0 bg-black/30 backdrop-blur-sm backdrop-brightness-50" />
-
-        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-            <DialogPanel className="max-w-lg space-y-4 bg-gray-800 p-12 rounded">
-            <DialogTitle className="font-bold"><h3 className="text-2xl">Fotos herunterladen</h3></DialogTitle>
-            <Description>
-                <p>Info: Für den Download benötigst du ein <b>Passwort</b>, welches wir in unserer Whatsapp -und Signalgruppe preisgeben.</p><br />
-                <p>Die Fotos speichern wir bei einem Schweizer Anbieter (Infomaniak / Swisstransfer)</p>
-            </Description>
-            <div className="flex gap-4 items-center mt-4">
-              {!!cube.swissTransferLink && <LinkButton className="mr-2" href={cube.swissTransferLink} onClick={() => onClosed()} type='full'>Herunterladen</LinkButton>}
-              <p className="text-red-500"><b>Link läuft in {cube.getDaysUntilSwissTransferLinkExpired()} Tage(n) ab!</b></p>
-            </div>
-            </DialogPanel>
-        </div>
+    <Dialog isOpen={isOpen} onClosed={() => onClosed()}>
+        <DialogTitle>Fotos herunterladen</DialogTitle>
+        <p>Info: Für den Download benötigst du ein <b>Passwort</b>, welches wir in unserer Whatsapp -und Signalgruppe preisgeben.</p><br />
+        <p>Die Fotos speichern wir bei einem Schweizer Anbieter (Infomaniak / Swisstransfer)</p>
+        <DialogActions>
+          {!!cube.swissTransferLink && <LinkButton className="mr-2" href={cube.swissTransferLink} onClick={() => onClosed()} type='full'>Herunterladen</LinkButton>}
+          <p className="text-red-500"><b>Link läuft in {cube.getDaysUntilSwissTransferLinkExpired()} Tage(n) ab!</b></p>
+        </DialogActions>
     </Dialog>
   );
 }
